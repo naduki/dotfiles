58714ba22a350c326a7fa471d0141def276efda6
naduki <68984205+naduki@users.noreply.github.com>
2025-07-18 21:04:24 +0900

    Refactor Hyprland configuration: enable quickshell, hyprlock, hypridle, and hyprshell; update package list

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -1,8 +1,38 @@
-{pkgs, ...}:
+{pkgs-stable, ...}:
 {
-  programs.hyprpanel.enable = true;
-  programs.rofi = {
-    enable = true;
-    package = pkgs.rofi-wayland;
+  programs = {
+    quickshell = {
+      enable = true;
+      # package = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    };
+    hyprlock = {
+      enable = true;
+      # package = inputs.hyprlock.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    };
   };
+  services = {
+    hypridle = {
+      enable = true;
+      # package = inputs.hypridle.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    };
+    hyprshell = {
+      enable = true;
+      systemd.enable = false;
+      # package = inputs.hyprshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    };
+  };
+
+  home.packages = with pkgs-stable; [
+    gnome-calculator
+
+    hyprshot
+    slurp
+    wl-screenrec
+
+    swaynotificationcenter
+    
+    mint-y-icons
+    nemo-with-extensions
+    xed-editor
+  ];
 }
