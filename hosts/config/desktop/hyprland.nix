2440925bdd2c2cbe1d764774749a3cce007f5378
naduki <68984205+naduki@users.noreply.github.com>
2025-07-19 20:06:50 +0900

    Update Hyprland configuration: enable Bluetooth support, add Polkit security, and override icon theme

--- hosts/config/desktop/hyprland.nix
+++ hosts/config/desktop/hyprland.nix
@@ -1,14 +1,26 @@
+{pkgs, ...}:
 {
   programs = {
     hyprland = {
       enable = true;
       withUWSM  = true;
     };
+    # nm-applet.enable = true;
   };
+  hardware.bluetooth.enable = true;
   services.gnome.gnome-keyring.enable = true;
+  security.polkit.enable = true;
   security.pam.services = {
     hyprlock = {};
   };
+  # Override packages
+  nixpkgs.config.packageOverrides = pkgs: {
+    colloid-icon-theme = pkgs.colloid-icon-theme.override { colorVariants = ["teal"]; };
+  };
+  environment.systemPackages = with pkgs; [
+    colloid-icon-theme
+  ];
+
   # Optional, hint Electron apps to use Wayland:
   # environment.sessionVariables.NIXOS_OZONE_WL = "1";
 }
