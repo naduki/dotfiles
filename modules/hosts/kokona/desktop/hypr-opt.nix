bbaf57b940cc01c23caa8fca55c07261f564f744
naduki <68984205+naduki@users.noreply.github.com>
2025-08-23 06:12:25 +0900

    Some Hyprland configuration split into hypr-opt.nix

---
+++ modules/hosts/kokona/desktop/hypr-opt.nix
@@ -0,0 +1,47 @@
+{ config, lib, pkgs, ... }:
+{
+  # Settings enabled on other desktops (Cinnamon, ...)
+  # gcr-ssh-agent setting (for cinnamon)
+  environment.extraInit = lib.optionalString config.services.gnome.gcr-ssh-agent.enable ''
+    if [ -z "$SSH_AUTH_SOCK" ] && [ -n "$XDG_RUNTIME_DIR" ]; then
+      export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gcr/ssh"
+    fi
+  '';
+  # Enable Bluetooth support
+  hardware.bluetooth.enable = true;
+  # Enable dconf
+  programs.dconf.enable = true;
+  # Security
+  security = {
+    polkit.enable = true;
+    pam.services.hyprland.enableGnomeKeyring = true;
+  };
+  # Services
+  services = {
+    blueman.enable = true;
+    dbus.enable = true;
+    gnome.gnome-keyring.enable = true;
+    gvfs.enable = true;
+  };
+  # xdg.portal
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-xapp
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
+    ];
+    config = {
+      hyprland = {
+        default = [
+          "hyprland"
+          "xapp"
+          "gtk"
+        ];
+        "org.freedesktop.impl.portal.Secret" = [
+          "gnome-keyring"
+        ];
+      };
+    };
+  };
+}
--- modules/hosts/kokona/desktop/hyprland.nix
+++ modules/hosts/kokona/desktop/hyprland.nix
@@ -1,62 +1,18 @@
-{ config, lib, myconf, pkgs, ... }:
+{ config, lib, myconf, ... }:
 {
+  # Load additional settings only when Hyprland is the sole environment
+  # Given the condition under which this file is imported,
+  # myconf.environmenthaving length 1 indicates Hyprland is the only environment.
+  imports = lib.optionals (builtins.length myconf.environment == 1) [ ./hypr-opt.nix ];
+
   # Enable Hyprland desktop environment.
   programs.hyprland.enable = true;
   # Enable Hyprlock to unlock from Home-manager
   security.pam.services.hyprlock = {};
   # Fix Suspend/wakeup issues with Hyprland
-  # Because this uses open drivers, this may not resolve the issue.
-  hardware.nvidia.powerManagement.enable = true;
+  hardware.nvidia.powerManagement.enable = (lib.lists.elem "nvidia" config.services.xserver.videoDrivers);
 
   environment.systemPackages = [
     # pkgs.kitty   # If you don't have a terminal, be sure to install it
   ];
-
-  # Enable these if services.xserver.desktopManager.cinnamon.enable is false
-  # gcr-ssh-agent setting (for cinnamon)
-  environment.extraInit = lib.optionalString config.services.gnome.gcr-ssh-agent.enable ''
-    # Hack: https://bugzilla.redhat.com/show_bug.cgi?id=2250704 still
-    # applies to sessions not managed by systemd.
-    if [ -z "$SSH_AUTH_SOCK" ] && [ -n "$XDG_RUNTIME_DIR" ]; then
-      export SSH_AUTH_SOCK="$XDG_RUNTIME_DIR/gcr/ssh"
-    fi
-  '';
-  # Enable Bluetooth support
-  hardware.bluetooth.enable = true;
-  # Enable dconf
-  programs.dconf.enable = true;
-  # Security
-  security = {
-    polkit.enable = true;
-    pam.services.hyprland.enableGnomeKeyring = true;
-  };
-  # Services
-  services = {
-    blueman.enable = true;
-    dbus.enable = true;
-    gnome.gnome-keyring.enable = true;
-    gvfs.enable = true;
-  };
-  # xdg.portal
-  # From the import condition of this file, when the WM is only Hyprland, the number of elements in myconf.desktop is 1
-  xdg.portal = lib.mkIf (builtins.length myconf.environment == 1) {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-xapp
-      xdg-desktop-portal-hyprland
-      xdg-desktop-portal-gtk
-    ];
-    config = {
-      hyprland = {
-        default = [
-          "hyprland"
-          "xapp"
-          "gtk"
-        ];
-        "org.freedesktop.impl.portal.Secret" = [
-          "gnome-keyring"
-        ];
-      };
-    };
-  };
 }
