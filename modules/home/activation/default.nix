d1f9e2cc415c5730b9c9d749fc1441ede8f63a60
naduki <68984205+naduki@users.noreply.github.com>
2025-09-07 23:49:50 +0900

    Remove Zed editor config and activation module

--- modules/home/activation/default.nix
+++ modules/home/activation/default.nix
@@ -4,7 +4,6 @@
   ## This is effectively an --impure state
   imports = [
     ./brave-cache.nix
-    ./zed-config.nix
   ]
   ++ lib.optional (builtins.elem "Hyprland" (myconf.environment or [ ])) ./hypr-custom.nix;
 }
--- modules/home/activation/zed-config.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, myconf, ... }:
-{
-  # Create a symbolic link for Zed Global Setting (Because it must be able to be rewritten with AI settings)
-  config = lib.mkIf (config.programs.zed-editor.enable or false) {
-    home.activation.makeZedSymbolic = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-      run cp -r ${myconf.flakeRoot}/config/zed ${config.home.homeDirectory}/.config/zed
-    '';
-  };
-}
