c8647351d440076a6311ddd6e227b85a42e8971e
naduki <68984205+naduki@users.noreply.github.com>
2026-02-11 23:02:41 +0900

    feat: Configure Caps Lock as Escape and adjust Picture-in-Picture window geometry in Sway, and add Free and DejaVu font families.

--- modules/home/naduki/fonts.nix
+++ modules/home/naduki/fonts.nix
@@ -6,15 +6,21 @@
         "Klee One"
         "LXGW WenKai Light"
         "Noto Sans CJK JP"
+        "FreeSans"
+        "DejaVu Sans"
       ];
       serif = [
         "Klee One"
         "LXGW WenKai Light"
         "Noto Serif CJK JP"
+        "FreeSerif"
+        "DejaVu Serif"
       ];
       monospace = [
         "Moralerspace Radon HW"
         "LXGW WenKai Mono"
+        "FreeMono"
+        "DejaVu Sans Mono"
       ];
       emoji = [
         "Noto Color Emoji"
